1385c01a2d41cfd22e1accb86952d6692e4c1521
dekkermg <dekker.mg@outlook.com>
2017-05-22 14:42:50 +0200

    werkende demo versie. Ophalen uit mongo werkt nog niet
    werkende demo versie. Ophalen uit mongo werkt nog niet

--- mongoDbConnection.js
+++ mongoDbConnection.js
@@ -18,4 +18,21 @@ module.exports = function() {
       });
     });
   };
+
+  this.findAll = function() {
+    var rides;
+    console.log("start verbinding met db");
+    // Set up the connection to the local db
+    var records = MongoClient.connect("mongodb://localhost:27017/rijschema", function(err, database){
+      if(err) console.log("er gaat iets mis met mongo connectie maken")
+      else console.log("verbinding met mongodb gemaakt")
+      console.log("start findAll");
+      rides = database.collection('rides').find().toArray(function(err, docs){
+        //console.log(JSON.stringify(docs));
+        return docs;
+      });
+      console.log(JSON.stringify(rides));
+    });
+    return rides;
+  };
 };
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var formidable = require('formidable');
+var bodyParser = require('body-parser');
 var util = require('util');
 var uuid = require('node-uuid');
 var express = require('express');
 var mongoDbConnnection = require('./mongoDbConnection');
 
 var app = express();
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -20,13 +24,17 @@ app.get('/', function(req, res) {
 });
 
 app.get('/rides', function(req, res) {
+        console.log("entered rides");
+        var db = new mongoDbConnnection();
+        //var data = db.findAll();
         var data = [{               
                     "id": uuid.v1(),
                     "name": "Noordwijk uit",
                     "date": "19 Augustus, 2016",
                     "description": "Uit tegen Noordwijk",
                     "participantType": "rijder",
-                    "nrOfSeats": 3
+                    "drivers": [{"name": "Adriaan", "nrOfSeats": 3}],
+                    "companions": [{"name": "Martin", "remarks": "geen"}]
                     },
                     {
                     "id": uuid.v1(),
@@ -34,94 +42,29 @@ app.get('/rides', function(req, res) {
                     "date": "18 September, 2016",
                     "description": "Uit tegen ASC",
                     "participantType": "meerijder",
-                    "nrOfSeats": 1
+                    "drivers": [{"name": "Adriaan", "nrOfSeats": 3}],
+                    "companions": [{"name": "Martin", "remarks": "geen"}]
         }];
-
+        
         var responseData = JSON.stringify(data);
         res.end(responseData);
-        console.log("get: ", responseData);
+        console.log("get responseData: ", responseData);
         return;
 });
 
-app.post('/ride', function(req, res) {
-    var form = new formidable.IncomingForm();
-
-    form.parse(req, function (err, fields) {
-
+app.post('/ride', jsonParser, function(req, res) {
         res.writeHead(200, {
             'content-type': 'text/plain'
         });
+        var jsonRide = JSON.stringify(req.body, null, 2);
 
-        console.log('posted fields:\n');
-        console.log(fields);
+        console.log('posted ride in JSON:\n');
+        console.log(jsonRide);
 
         var db = new mongoDbConnnection();
-        var data = db.insert(fields);        
-        res.end(data);
-    });
+        var data = db.insert(req.body);        
+        res.end(jsonRide);
 })
 
 app.listen(3100);
 console.log('Express-server gestart op http://localhost:3100');
-
-/*
-
-var server = http.createServer(function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
-
-    if (req.method.toLowerCase() == 'post') {
-        processForm(req, res);
-        return;
-    }
-
-    if (req.method.toLowerCase() == 'get') {
-        var data = [{               
-                    "id": uuid.v1(),
-                    "name": "Noordwijk uit",
-                    "date": "19 Augustus, 2016",
-                    "description": "Uit tegen Noordwijk",
-                    "participantType": "rijder",
-                    "nrOfSeats": 3
-                    },
-                    {
-                    "id": uuid.v1(),
-                    "name": "ASC uit",
-                    "date": "18 September, 2016",
-                    "description": "Uit tegen ASC",
-                    "participantType": "meerijder",
-                    "nrOfSeats": 1
-        }];
-
-        var responseData = JSON.stringify(data);
-        res.end(responseData);
-        console.log("get: ", responseData);
-        return;
-    }
-    res.end();
-});
-
-function processForm(req, res) {
-    var form = new formidable.IncomingForm();
-
-    form.parse(req, function (err, fields) {
-
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-
-        var data = JSON.stringify({
-            fields: fields
-        });
-        
-        res.end(data);
-
-        console.log('posted fields:\n');
-        console.log(data);
-    });
-}
-
-var port = 3100;
-server.listen(port);
-console.log('server listening on port' + port);
-*/
